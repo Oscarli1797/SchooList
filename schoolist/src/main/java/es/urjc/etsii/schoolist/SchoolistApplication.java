c52c6a2d3de502275ff253c77c0295aba5426cf3
MaJiGerman <43379084+MaJiGerman@users.noreply.github.com>
2019-01-29 14:01:54 +0100

    recreado el proyecto en eclipse sts

---
+++ schoolist/src/main/java/es/urjc/etsii/schoolist/SchoolistApplication.java
@@ -0,0 +1,14 @@
+package es.urjc.etsii.schoolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolistApplication.class, args);
+	}
+
+}
+
---
+++ schoolist/src/test/java/es/urjc/etsii/schoolist/SchoolistApplicationTests.java
@@ -0,0 +1,17 @@
+package es.urjc.etsii.schoolist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
