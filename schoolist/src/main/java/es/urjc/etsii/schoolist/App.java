b855eed2b4e38d63c0ce4b84ba7156c13bd348ef
MaJiGerman <43379084+MaJiGerman@users.noreply.github.com>
2019-01-29 13:15:01 +0100

    creacion del proyecto
    creacion de la base del proyecto maven

---
+++ schoolist/src/main/java/es/urjc/etsii/schoolist/App.java
@@ -0,0 +1,13 @@
+package es.urjc.etsii.schoolist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
