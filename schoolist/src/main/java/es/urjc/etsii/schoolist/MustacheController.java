1dce0149d0ef4298228bade8f19b6c95bf9af00c
Angelitorl <an1997991@gmail.com>
2019-01-30 19:22:30 +0100

    Mini ejemplo
    Añadido UTF-8
    Hecho pequeño ejemplo de transicion entre htmls

--- schoolist/src/main/java/es/urjc/etsii/schoolist/MustacheController.java
+++ schoolist/src/main/java/es/urjc/etsii/schoolist/MustacheController.java
@@ -1,8 +1,23 @@
 package es.urjc.etsii.schoolist;
 
-import org.springframework.stereotype.Controller;//nani??
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MustacheController 
 {
+	
+	@RequestMapping(value={"", "/", "home"})
+	 public String base(Model model) {
+		model.addAttribute("name", "home");
+		return "home_template";
+	 }
+	
+	@RequestMapping("/login")
+	 public String greeting(Model model) {
+		model.addAttribute("name", "login");
+		return "login_template";
+	 }
+	
 }
