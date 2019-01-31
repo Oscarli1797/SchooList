d322a572d0a220f290912c6b5ef1ed415cca7f7f
German Matilla Jimenez <g.matilla@alumnos.urjc.es>
2019-01-31 11:55:02 +0100

    templates de navegacion basica
    creada la funcionalidad basica de navegacion, las templates de mustache y  el controlador

--- schoolist/src/main/java/es/urjc/etsii/schoolist/MustacheController.java
+++ schoolist/src/main/java/es/urjc/etsii/schoolist/MustacheController.java
@@ -20,4 +20,34 @@ public class MustacheController
 		return "login_template";
 	 }
 	
+	@RequestMapping("/admin")
+	 public String admin(Model model) {
+		model.addAttribute("name", "admin");
+		return "admin_template";
+	 }
+	
+	@RequestMapping("/profesor")
+	 public String profesor(Model model) {
+		model.addAttribute("name", "profesor");
+		return "profesor_template";
+	 }
+	
+	@RequestMapping("/padre")
+	 public String padre(Model model) {
+		model.addAttribute("name", "padre");
+		return "padre_template";
+	 }
+	
+	@RequestMapping("/monitor")
+	 public String monitor(Model model) {
+		model.addAttribute("name", "monitor");
+		return "monitor_template";
+	 }
+	
+	@RequestMapping("/mail")
+	 public String mail(Model model) {
+		model.addAttribute("name", "mail");
+		return "mail_template";
+	}
+	
 }
