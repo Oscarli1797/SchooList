fa0775888049f3d75fa65e682a5bf61d03dd3afc
MaJiGerman <pedroa786@gmail.com>
2019-02-25 19:28:03 +0100

    Update iniciarBases.sql
    nueva query con la inicializacion teniendo al padre dentro de alumno

--- Documentacion/iniciarBases.sql
+++ Documentacion/iniciarBases.sql
@@ -20,29 +20,29 @@ insert into asignatura_grupo values ('100001', '179431');
 insert into asignatura_grupo values ('100002', '179431');
 insert into asignatura_grupo values ('100003', '179431');
 
-insert into usuario values ('Admin', 'carsena', 'Serrano', 'Navia', 'Carlos', '1234', null, null);
-insert into usuario values ('Admin', 'fersena', 'Serrano', 'Navia', 'Fernando', '1234', null, null);
+insert into usuario values ('Admin', 'carsena', 'Serrano', 'Navia', 'Carlos', '1234', null);
+insert into usuario values ('Admin', 'fersena', 'Serrano', 'Navia', 'Fernando', '1234', null);
 
-insert into usuario values ('Profesor', 'jureher', 'Reyes', 'Hernandez', 'Julia', '1234', null, null);
-insert into usuario values ('Profesor', 'clausepas', 'Serrano', 'Pastor', 'Claudia', '1234', null, null);
+insert into usuario values ('Profesor', 'jureher', 'Reyes', 'Hernandez', 'Julia', '1234', null);
+insert into usuario values ('Profesor', 'clausepas', 'Serrano', 'Pastor', 'Claudia', '1234', null);
 
-insert into usuario values ('Monitor', 'frandiazvi', 'Diaz', 'de Vivar', 'Francisco', '1234',  null, '12134425');
-insert into usuario values ('Monitor', 'fegarruiz', 'Garcia', 'Ruiz', 'Federico', '1234',  null, null);
+insert into usuario values ('Monitor', 'frandiazvi', 'Diaz', 'de Vivar', 'Francisco', '1234', '12134425');
+insert into usuario values ('Monitor', 'fegarruiz', 'Garcia', 'Ruiz', 'Federico', '1234', null);
 
 insert into usuario_asignaturas values ('jureher', '100000');
 insert into usuario_asignaturas values ('jureher', '100001');
 insert into usuario_asignaturas values ('clausepas', '100002');
 insert into usuario_asignaturas values ('clausepas', '100003');
 
-insert into alumno values('12345678', '1234567', 'Zamora', 'Peiro', 'Ester', '179431', '11111113');
-insert into alumno values('12345679', '1345636', 'Corazon', 'Leon', 'Ricardo', '179431', '11111112');
-insert into alumno values('12345680', '6536563', 'Cruz', 'Garcia', 'Sergio', '179431', '11111111');
-insert into alumno values('12345674', '4674256', 'Mejilla', 'Jimenez', 'Carlos', '179431', '11111114');
+insert into usuario values ('Padre', 'silzavi', 'Zamora', 'de Vivar', 'Silvia', '1234', null);
+insert into usuario values ('Padre', 'rismecal', 'Mejilla', 'Calvo', 'Risto', '1234', null);
+insert into usuario values ('Padre', 'sercopal', 'Corazon', 'Palpito', 'Sergio', '1234', null);
+insert into usuario values ('Padre', 'caacruz', 'Cruz', 'Amaya', 'Carolina', '1234', null);
 
-insert into usuario values ('Padre', 'silzavi', 'Zamora', 'de Vivar', 'Silvia', '1234', '12345678', null);
-insert into usuario values ('Padre', 'rismecal', 'Mejilla', 'Calvo', 'Risto', '1234', '12345674', null);
-insert into usuario values ('Padre', 'sercopal', 'Corazon', 'Palpito', 'Sergio', '1234', '12345679', null);
-insert into usuario values ('Padre', 'caacruz', 'Cruz', 'Amaya', 'Carolina', '1234', '12345680', null);
+insert into alumno values('12345678', '1234567', 'Zamora', 'Peiro', 'Ester', '179431', 'rismecal', '11111113');
+insert into alumno values('12345679', '1345636', 'Corazon', 'Leon', 'Ricardo', '179431', 'rismecal', '11111112');
+insert into alumno values('12345680', '6536563', 'Cruz', 'Garcia', 'Sergio', '179431', 'sercopal', '11111111');
+insert into alumno values('12345674', '4674256', 'Mejilla', 'Jimenez', 'Carlos', '179431', 'caacruz', '11111114');
 
 insert into falta values ('0000001', '2019-01-24', 'mi hij@ esta enganchado a un arbol, no va a poder ir', '12345678');
 
